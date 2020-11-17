191cb8adb8156eb7a45c456062a96cffc459a1cb
Official21A <officialamirhossein21@gmail.com>
2020-11-17 07:55:19 +0330

    search

--- index.py
+++ index.py
@@ -1,13 +1,21 @@
+import googlesearch
 from googlesearch import search
 
+print(">> User agant :")
+print(googlesearch.get_random_user_agent())
 
-response = list(search("python", num=10, stop=10, pause=2))
+response = list(search("python", num=10, stop=10, pause=2, safe="on"))
 
-for result in response:
-    print(result)
+
+with open('python.txt', "w") as file:
+    for result in response:
+    	file.write(result + "\n")
   
 # to search 
 query = "A computer science portal"
-  
-for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-    print(j) 
+
+with open('test.txt', 'w') as file:  
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+    	file.write(j + "\n") 
+
+print("Done")
