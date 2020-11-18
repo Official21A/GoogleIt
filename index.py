b990f2b9ad6d2b3798cd501a3a8d65527670f8aa
Official21A <officialamirhossein21@gmail.com>
2020-11-18 07:46:33 +0330

    google problem

--- index.py
+++ index.py
@@ -1,31 +1,51 @@
 import googlesearch
 from googlesearch import search
+import sys
 
 
-# First we give a information about user agent
-print(">> User agant :")
-print(googlesearch.get_random_user_agent())
+def user_agent():
+	# Information about user agent
+	print(">> User agant :")
+	print(googlesearch.get_random_user_agent())
 
 
-# Opening a connection and getting a list of search
-try:
-	response = list(search("python", num=10, stop=10, pause=2, safe="on"))
-except (e,):
-	print(f"> Error in {e}") # connection handeling
+def safe_search():
+	# Opening a connection and getting a list of search
+	try:
+		response = list(search("python", num=10, stop=10, pause=2, safe="on"))
+	except Exception as e:
+		print(f"> Error in :: {e}") # connection handeling
+		response = []
 
-with open('python.txt', "w") as file: # save them into a file
-    for result in response:
-    	file.write(result + "\n")
+	with open('python.txt', "w") as file: # save them into a file
+		for result in response:
+			file.write(result + "\n")
   
-# to search in an other way
-query = "A computer science portal"
 
-with open('test.txt', 'w') as file:
-    try:  
-    	for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
-    		file.write(j + "\n") 
-    except (e,):
-    	print(f"> Error in {e}")
+def search():
+	# To search in an normal way
+    query = "A computer science portal"
+
+    with open('test.txt', 'w') as file:
+    	try:  
+    		for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+    			file.write(j + "\n") 
+    	except Exception as e:
+    		print(f"> Error in :: {e}")
 
 
-print("Done")
+def execute(safe="off"):
+	# Program executig
+	if safe == "on":
+		safe_search()
+	else:
+		search()
+	print("Done")
+
+
+# Program starts
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		execute()
+	else:
+		execute("on")
