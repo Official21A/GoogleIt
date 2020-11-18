4f6c1a96804b71378b3c132cefebc0da6c5f2b27
Official21A <officialamirhossein21@gmail.com>
2020-11-18 08:19:22 +0330

    refactor

--- index.py
+++ index.py
@@ -1,36 +1,5 @@
-import googlesearch
-from googlesearch import search
-
 import sys
-
-
-def user_agent():
-	# Information about user agent
-	print(">> User agant :")
-	print(googlesearch.get_random_user_agent())
-
-
-def safe_search(query, bound, output):
-	# Opening a connection and getting a list of search
-	try:
-		response = list(search(query, num=10, stop=10, pause=bound, safe="on"))
-	except Exception as e:
-		print(f"> Error in :: {e}") # connection handeling
-		response = []
-
-	with open(f'{output}.txt', "w") as file: # save them into a file
-		for result in response:
-			file.write(result + "\n")
-  
-
-def normal_search(query, bound, output):
-	# To search in an normal way
-    with open(f'{output}.txt', 'w') as file:
-    	try:  
-    		for j in search(query, tld="com", num=10, stop=10, pause=bound): 
-    			file.write(j + "\n") 
-    	except Exception as e:
-    		print(f"> Error in :: {e}")
+from headers import safe_search, normal_search, user_agent
 
 
 def command_input(argumenst):
