24faf1fbac7c36efd3322b254f815125cc7d3909
Official21A <officialamirhossein21@gmail.com>
2020-11-19 10:00:59 +0330

    firewall

--- firewall.py
+++ firewall.py
@@ -9,44 +9,26 @@ def privacy_policy(url):
 	browser = mechanicalsoup.StatefulBrowser()
 	try:
 		browser.open(url, timeout=1)
-	except:
-		print(">> privacy policy checking failed.")
+	except Exception as e:
+		print(f">> privacy policy checking failed (url opening). {e}")
 		return False
-	page = browser.get_current_page() 
 	try:
-		links = page.findAll('a')
-	except:
-		print(">> privacy policy checking failed.")
+		page = browser.get_current_page() 
+		if page.title.startswith("40"):
+			raise Exception(":: Error in domain.")
+	except Exception as e:
+		print(f">> privacy policy checking failed (scanning). {e}")
 		return False
+	links = page.findAll('a')
 	inner_links = [a.text.lower() for a in links]
 	return "privacy policy" in inner_links
 
 
 def active(url):
-	print(">> {}".format(url.strip()))
 	https_result = https_check(url)
-	print(https_result)
 	pp_result = privacy_policy(url)
-	print(pp_result)
-	print()
 	return https_result or pp_result
 
 
 def checkin(url_list):
 	return [ url for url in url_list if active(url)]
-
-
-
-def test_bench():
-	with open("./docs/google.txt", 'r') as file:
-		lines = file.readlines()
-		urls = [line for line in lines]
-		print("> File read:")
-		print(urls)
-		urls = checkin(urls)
-		print("> Output:")
-		print(urls)
-
-
-if __name__ == "__main__":
-	test_bench()
