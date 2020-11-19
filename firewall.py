399f49b76c4012843aae04e2524f064412f01c83
Official21A <officialamirhossein21@gmail.com>
2020-11-19 09:31:58 +0330

    pp check bug

--- firewall.py
+++ firewall.py
@@ -8,10 +8,41 @@ def https_check(domain):
 def privacy_policy(url):
 	browser = mechanicalsoup.Browser()
 	try:
-		page = browser.get(url)
+		browser.open(url, timeout=5)
 	except:
+		print(">> privacy policy checking failed.")
 		return False
-	page_soup = page.soup 
+	page_soup = browser.page.soup 
 	links = page_soup.findAll('a')
-	inner_links = [a.text.lowwer() for a in links]
+	inner_links = [a.text.lower() for a in links]
 	return "privacy policy" in inner_links
+
+
+def active(url):
+	print(">> {}".format(url.strip()))
+	https_result = https_check(url)
+	print(https_result)
+	pp_result = privacy_policy(url)
+	print(pp_result)
+	print()
+	return https_result or pp_result
+
+
+def checkin(url_list):
+	return [ url for url in url_list if active(url)]
+
+
+
+def test_bench():
+	with open("./docs/google.txt", 'r') as file:
+		lines = file.readlines()
+		urls = [line for line in lines]
+		print("> File read:")
+		print(urls)
+		urls = checkin(urls)
+		print("> Output:")
+		print(urls)
+
+
+if __name__ == "__main__":
+	test_bench()
